3fb6fdacc372c18b51932122280272a08fc7ddb6
pocketparent <dianamezzanotte@gmail.com>
2025-04-14 12:44:13 -0400

    Update firebase.py

--- backend/utils/firebase.py
+++ backend/utils/firebase.py
@@ -1,19 +1,21 @@
-import firebase_admin
-from firebase_admin import credentials, firestore, storage
 import os
+import firebase_admin
+from firebase_admin import credentials
 
-# Avoid re-initializing if already done
 if not firebase_admin._apps:
     cred = credentials.Certificate({
         "type": "service_account",
-        "project_id": os.environ["FIREBASE_PROJECT_ID"],
-        "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
-        "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
-        "token_uri": "https://oauth2.googleapis.com/token"
-    })
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': os.environ["FIREBASE_STORAGE_BUCKET"]
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
     })
 
-db = firestore.client()
-bucket = storage.bucket()
+    firebase_admin.initialize_app(cred, {
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET")
+    })
