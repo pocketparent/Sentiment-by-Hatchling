2a0cada435f31af74663f0467da6e1b31303a50d
pocketparent <dianamezzanotte@gmail.com>
2025-04-13 22:12:22 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
   },
   build: {
-    outDir: 'dist'
-  }
-})
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
