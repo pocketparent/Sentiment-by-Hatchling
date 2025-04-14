ed683f4c6dbec4366536219723679b3a399a830a
pocketparent <dianamezzanotte@gmail.com>
2025-04-13 21:04:02 -0400

    Delete frontend_src directory

--- frontend_src/App.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default function App() { return <div>Hatchling App</div>; }
--- frontend_src/components/EntryModal.tsx
+++
@@ -1,1 +0,0 @@
-export default function EntryModal() { return <div>Entry Modal</div>; }
