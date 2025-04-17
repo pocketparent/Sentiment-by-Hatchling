fd69bfe64a7983fe8294706e5a92b75a68a5a242
pocketparent <dianamezzanotte@gmail.com>
2025-04-16 20:01:47 -0400

    Delete backend directory

--- backend/utils/sms.py
+++
@@ -1,9 +0,0 @@
-# utils/sms.py
-from twilio.rest import Client
-import os
-
-def send_login_link(phone, token):
-    client = Client(os.getenv("TWILIO_SID"), os.getenv("TWILIO_AUTH"))
-    url = f"https://myhatchling.ai/login?token={token}"
-    message = f"Tap to log in to Hatchling: {url}"
-    client.messages.create(to=phone, from_=os.getenv("TWILIO_NUMBER"), body=message)
